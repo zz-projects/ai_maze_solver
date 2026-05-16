8d25df4a358bbf2517c6ddb09d5b12cd1bd8120e
ZZ <219213377+zz-projects@users.noreply.github.com>
2026-05-16 18:56:34 +0330

    Update main.py -- 2 types of DFS added: Graph DFS and Path-Based DFS.

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from collections import deque
 
-maze = [["S", ".", ".","#","."],
+
+maze = [["S",".",".","#","."],
         ["#","#",".","#","."],
         [".",".",".",".","."],
         [".","#","#","#","."],
         [".",".",".","G","."]
 ]
 
-maze2 = [["S", ".", ".",".","#","."],
-        ["#","#",".",".","#","."],
-        [".",".",".",".",".","."],
-        [".","#","#","#","#","."],
-        [".",".",".","G",".","."]
+
+maze2 = [["S",".",".",".","#","."],
+         ["#","#",".",".","#","."],
+         [".",".",".",".",".","."],
+         [".","#","#","#","#","."],
+         [".",".",".","G",".","."]
 ]
 #"S" = Start
 #"G" = Goal
@@ -24,11 +26,11 @@ def find_position(maze, symbol):
     This function takes the maze and S/G and
     returns the position of the S/G as output.
     '''
-
-    for x_loc,each_row_list in enumerate(maze):
-        for y_loc,item in enumerate(each_row_list):
+    # row, col = column
+    for row,each_row_list in enumerate(maze):
+        for col,item in enumerate(each_row_list):
             if item == symbol:
-                position = (x_loc,y_loc)
+                position = (row,col)
                 return position
 #Test:
 #print(find_position(maze, "G"))
@@ -39,16 +41,18 @@ def get_neighbors(position, maze):
     and returns neighbors' positions that are possible to go to as output.
     It trys to stay inside the grid and avoid walls.
     '''
-    x,y = position
-    moves = [(-1,0),(1,0),(0,1),(0,-1)]
+    row,col = position
+    # up - right - down - left
+    moves = [(-1,0),(0,1),(1,0),(0,-1)]
     neighbors = []
 
-    for dx,dy in moves:
-        nx, ny = x+dx, y+dy
+    # dr=d_row | dc=d_column | nr=new row | nc=new column
+    for dr,dc in moves:
+        nr, nc = row+dr, col+dc
 
-        if 0<=nx<len(maze) and 0<=ny<len(maze[0]):
-            if maze[nx][ny] != "#":
-                neighbors.append((nx,ny))   
+        if 0<=nr<len(maze) and 0<=nc<len(maze[0]):
+            if maze[nr][nc] != "#":
+                neighbors.append((nr,nc))   
 
     return neighbors
 #Test:
@@ -64,10 +68,11 @@ def bfs(maze, start, goal):
     queue.append((start, [start]))
     #print("queue now is:", queue)
     visited = set()
-
+    
     # continue as long as there are positions to explore
     while queue:
         max_queue_size = max(max_queue_size, len(queue))
+        #FIFO
         current, path = queue.popleft()
         nodes_exploered += 1
         #print("current is:", current)
@@ -97,9 +102,7 @@ def bfs(maze, start, goal):
                 queue.append((neighbor, path + [neighbor]))
                 #print("neighbor of current is:", neighbor)
                 #print("new queue is:", queue)
-        
-
-
+                
 def bfs_optimized(maze, start, goal):
     '''
     In this optimized version of BFS, 2 other options are added:
@@ -120,6 +123,7 @@ def bfs_optimized(maze, start, goal):
     # continue as long as there are positions to explore
     while queue:
         max_queue_size = max(max_queue_size, len(queue))
+        #FIFO
         current, path = queue.popleft()
         nodes_exploered += 1
         #print("current is:", current)
@@ -145,6 +149,90 @@ def bfs_optimized(maze, start, goal):
                     visited.add(neighbor)
                     #print("neighbor of current is:", neighbor)
 
+def dfs_graph(maze, start, goal):
+    '''
+    This function uses graph DFS.
+    '''
+    step_counter = 0
+    nodes_exploered = 0
+    max_stack_size = 0
+    print("START DFS")
+    stack = deque()
+    stack.append((start,[start]))
+    visited = set()
+
+    while stack:
+        #Emergency Stop
+        step_counter+=1
+        if step_counter> 100:
+            print("Emergency Stop!")
+            break
+
+        max_stack_size = max(max_stack_size, len(stack))
+        #LIFO
+        current, path = stack.pop()
+        nodes_exploered += 1
+        print("current is:", current)
+        print("path now is:", path)
+        print("stack is :", stack)
+
+        visited.add(current)
+        # Goal test (DFS's goal test is right after choosing the node)
+        if current == goal:
+            print("Goal achieved!")
+            print("number of explored nodes:", nodes_exploered)
+            print("max stack size = ", max_stack_size)
+            return path        
+        
+        # keep preference of choosing neighbors: up - right - down - left
+        for neighbor in reversed(get_neighbors(current, maze)):
+            if neighbor not in visited:
+                print("neighbor is:", neighbor)
+                stack.append((neighbor, path + [neighbor]))
+                print("Stack Now:", stack)
+
+def dfs_path_based(maze, start, goal):
+    '''
+    This function uses path-based DFS.
+    No explored set, instead new nodes are checked with active path.
+    '''
+    step_counter = 0
+    nodes_exploered = 0
+    max_stack_size = 0
+    print("START Path-Based DFS")
+    stack = deque()
+    stack.append((start,[start]))
+
+    while stack:
+        #Emergency Stop
+        step_counter+=1
+        if step_counter> 100:
+            print("Emergency Stop!")
+            break
+
+        max_stack_size = max(max_stack_size, len(stack))
+        #LIFO
+        current, path = stack.pop()
+        nodes_exploered += 1
+        print("current is:", current)
+        print("path now is:", path)
+        print("stack is :", stack)
+
+        # Goal test (DFS's goal test is right after choosing the node)
+        if current == goal:
+            print("Goal achieved!")
+            print("number of explored nodes:", nodes_exploered)
+            print("max stack size = ", max_stack_size)
+            return path        
+        
+        # keep preference of choosing neighbors: up - right - down - left
+        for neighbor in reversed(get_neighbors(current, maze)):
+            if neighbor not in path:
+                print("neighbor is:", neighbor)
+                stack.append((neighbor, path + [neighbor]))
+                print("Stack Now:", stack)
+
+
 
 # display solution step-by-step
 def copy_maze(maze):
@@ -167,37 +255,58 @@ def print_maze(maze):
     for row in maze:
         print("  ".join(row))
 
-#Test1:
+#Test1 - smaller maze:
 start = find_position(maze, "S")
 goal = find_position(maze, "G")
+# BFS Test
 path1 = bfs(maze, start, goal)
-print("old path is: ", path1)
-# Test display:
+print("BFS path is: ", path1)
 marked = mark_path(maze,path1)
-print("\nSolved Maze:")
+print("\nSolved Maze - BFS:")
 print_maze(marked)
-#######
+# Optimized BFS Test
 path2 = bfs_optimized(maze, start, goal)
-print("new path is: ", path2)
-# Test display:
+print("Optimized BFS path is: ", path2)
 marked = mark_path(maze,path2)
-print("\nSolved Maze:")
+print("\nSolved Maze - BFS_Optimized:")
+print_maze(marked)
+# Graph DFS Test
+path3 = dfs_graph(maze, start, goal)
+print("Graph DFS path is:", path3)
+marked = mark_path(maze,path3)
+print("\nSolved Maze - Graph DFS:")
+print_maze(marked)
+# Path-Based DFS Test:
+path4 = dfs_path_based(maze, start, goal)
+print("Path-Based DFS is:", path4)
+marked = mark_path(maze,path4)
+print("\nSolved Maze - Path-Based DFS:")
 print_maze(marked)
 
-
-#Test2:
+#Test2 -- bigger maze:
 start = find_position(maze2, "S")
 goal = find_position(maze2, "G")
+# BFS Test:
 path1 = bfs(maze2, start, goal)
-print("old path is: ", path1)
-# Test display:
+print("BFS path is: ", path1)
 marked = mark_path(maze2, path1)
-print("\nSolved Maze:")
+print("\nSolved Maze - BFS:")
 print_maze(marked)
-#######
+# Optimized BFS Test
 path2 = bfs_optimized(maze2, start, goal)
-print("new path is: ", path2)
-# Test display:
+print("Optimized BFS path is: ", path2)
 marked = mark_path(maze2,path2)
-print("\nSolved Maze:")
+print("\nSolved Maze - Optimized BFS:")
+print_maze(marked)
+# Graph DFS Test
+path3 = dfs_graph(maze2, start, goal)
+print("Graph DFS path is:", path3)
+marked = mark_path(maze2,path3)
+print("\nSolved Maze - Graph DFS:")
+print_maze(marked)
+# Path-Based DFS Test:
+path4 = dfs_path_based(maze2, start, goal)
+print("Path-Based DFS is:", path4)
+marked = mark_path(maze2,path4)
+print("\nSolved Maze - Path-Based DFS:")
 print_maze(marked)
