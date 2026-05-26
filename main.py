919dddc279969a551f6ca9250330b5d128958b02
ZZ <219213377+zz-projects@users.noreply.github.com>
2026-05-26 19:48:14 +0330

    Update main.py --- parent-pointer version of UCS added. terrain_cost function updated for better efficiency.

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ maze4 = [
 # "M" = 7
 
 
-#print(f"This is maze:\n{maze}")
+
 
 def find_position(maze, symbol):
     '''
@@ -56,8 +56,6 @@ def find_position(maze, symbol):
             if item == symbol:
                 position = (row,col)
                 return position
-#Test:
-#print(find_position(maze, "G"))
 
 def get_neighbors(position, maze):
     '''
@@ -82,7 +80,6 @@ def get_neighbors(position, maze):
 #Test:
 #p1 = (4,4)
 #print(get_neighbors(p1,maze))
-
             
 def reconstruct_path(parents, start, goal):
     '''
@@ -108,19 +105,9 @@ def reconstruct_path(parents, start, goal):
     path.reverse()
     return path   
 
-def terrain_cost(position, maze):
-    '''
-    This function is needed when moves have different costs.
-    It will take the position and will return the cost of the move.
-    '''
-    row, col = position
-    if maze[row][col] in (".", "S", "G"):
-        cost = 1
-    elif maze[row][col] == "~":
-        cost = 3
-    elif maze[row][col] == "M":
-        cost = 7
-    return cost
+def terrain_cost(symbol):
+    costs = {".":1, "~":3, "M":7, "S":1, "G":1}
+    return costs[symbol]
 
 def bfs(maze, start, goal):
     '''
@@ -505,12 +492,81 @@ def ucs(maze, start, goal):
         for neighbor in get_neighbors(current, maze):
             if neighbor not in visited:
                 print("Neighbor is:", neighbor)
-                move_cost = terrain_cost(neighbor, maze)
+                row, col = neighbor
+                symbol = maze[row][col]
+                move_cost = terrain_cost(symbol)
                 new_node_cost = current_node_cost + move_cost
                 print("Current node cost:", current_node_cost)
                 print("Move Cost:", move_cost)
                 print("New node cost:", new_node_cost)
                 heapq.heappush(frontier, (new_node_cost, neighbor, path+[neighbor]))
+                print("frontier is :", frontier)
+
+def ucs_parent_pointer(maze, start, goal):
+    '''
+    This functions uses a standard version of UCS algorithm used for non-negative edge costs.
+    In this version, moves have different cost:
+    "." = 1
+    "~" = 3
+    "M" = 7
+    in this version instead of keeping the path as a list of tuples,
+    each node points to its parent, using a dictionary.
+    '''
+    print("START Parent-Pointer UCS")
+    step_counter = 0
+    nodes_explored = 0
+    max_frontier_size = 0
+    total_cost = 0
+    frontier = list()
+    parents = dict()
+    # Storing current position, total cost.
+    heapq.heappush(frontier, (total_cost, start, None))
+    parents[start] = None
+    visited = set()
+
+    # As long as frontier is not empty:
+    while frontier:
+        # Emergency Stop -- disable after tests
+        step_counter+=1
+        if step_counter> 100:
+            print("Emergency Stop!")
+            break
+
+        max_frontier_size = max(max_frontier_size, len(frontier))
+        # Least cost first
+        total_cost, current, parent= heapq.heappop(frontier)
+        current_node_cost = total_cost
+        print("Total cost is:", total_cost, "And current is:", current, "And parent is:", parent)
+        # As long as all moves' cost is positive this is okay, as each when each node is popped, ...
+        # the cheapest way to it has been found
+        if current in visited:
+            continue
+        # Assigning parent after popping to make sure that it is the best path
+        parents[current] = parent
+        nodes_explored+=1
+        print("current is:", current)
+        print("frontier is :", frontier)
+
+        visited.add(current)
+        # Goal test in UCS is after selecting a node
+        if current == goal:
+            path = reconstruct_path(parents, start, goal)
+            print("Goal achieved!")
+            print("number of explored nodes:", nodes_explored)
+            print("max frontier size = ", max_frontier_size)
+            return path   
+        
+        for neighbor in get_neighbors(current, maze):
+            if neighbor not in visited:
+                print("Neighbor is:", neighbor)
+                row, col = neighbor
+                symbol = maze[row][col]
+                move_cost = terrain_cost(symbol)
+                new_node_cost = current_node_cost + move_cost
+                print("Current node cost:", current_node_cost)
+                print("Move Cost:", move_cost)
+                print("New node cost:", new_node_cost)
+                heapq.heappush(frontier, (new_node_cost, neighbor, current))
                 print("frontier is :", frontier)
 
 # display solution step-by-step
@@ -684,3 +740,9 @@ goal = find_position(maze4, "G")
 #marked = mark_path(maze4,path8)
 #print("\nSolved Maze - UCS(different move cost):")
 #print_maze(marked)
+# UCS Parent-Pointer Test
+path9 = ucs_parent_pointer(maze4, start, goal)
+print("UCS (different move cost)-Parent-Pointer path is: ", path9)
+marked = mark_path(maze4,path9)
+print("\nSolved Maze - UCS(different move cost)-Parent-Pointer:")
+print_maze(marked)
